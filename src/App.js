7486671801aeae50298308f5d5d9eb58245708ba
rayan <>
2020-05-06 21:50:56 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+import { CardList } from './components/card-list/card-list-component';
+import { SearchBox } from './components/search-box/search-box.component';
+
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+  
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters:users}))
+  }
+
+  render(){
+    const {monsters,searchField} = this.state;
+    const filteredmonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
+      return(
+      <div className="App">
+        <h1 className='App-logo'>Monster Reloaded</h1>
+        <SearchBox placeholder={'search monster'} 
+        handleChange={e => this.setState({searchField:e.target.value})}/>
+        <CardList monsters={filteredmonsters}/>
+      </div>
+    );
+  }
+}
+
+export default App;
