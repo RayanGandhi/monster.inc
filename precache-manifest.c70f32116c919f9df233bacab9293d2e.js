996c5fd52c2b14bb913a2095ad6748951d9a8962
rayan <>
2020-05-06 22:00:02 +0530

    Updates

---
+++ precache-manifest.c70f32116c919f9df233bacab9293d2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66cdfaaeb4311f72229e14e2856f6a51",
+    "url": "/monster.inc/index.html"
+  },
+  {
+    "revision": "561c9555d46e9a98583f",
+    "url": "/monster.inc/static/css/main.1f162a2e.chunk.css"
+  },
+  {
+    "revision": "bf9573e74ea7b33df8b8",
+    "url": "/monster.inc/static/js/2.d205389b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster.inc/static/js/2.d205389b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "561c9555d46e9a98583f",
+    "url": "/monster.inc/static/js/main.0f0e0617.chunk.js"
+  },
+  {
+    "revision": "9d7cacd9a7f3be261881",
+    "url": "/monster.inc/static/js/runtime-main.8222fd60.js"
+  }
+]);
